c660c2000b96c500bc5214bd32f9f3ed676dabd8
Raphael Vogt <raphael.vogt@gmx.net>
2024-01-22 13:48:13 +0100

    added moebius transformations and distances from the center

--- hyperbolic/poincare.py
+++ hyperbolic/poincare.py
@@ -59,3 +59,56 @@ def geodesic(z1: TensorType[1], z2: TensorType[1], t, model="disk"):
 
     else:
         raise NotImplementedError("The model must be the Poincare Disk ('disk') or Lobachevksy half plane('plane')")
+
+
+def distance_center(z: TensorType[1], model="disk"):
+    """Computes the Riemann distance between two points in the Poincare Disk
+
+    Args:
+        z1 (complex tensor): 
+                    Specifies the point.
+    
+    Returns:
+        distance (float): 
+                    The computed distance.
+    """
+    # ensure that numbers are complex
+    if z.dtype is not torch.complex64:
+        raise TypeError("The number must be complex!")
+    
+    # compute distance on poincare disk
+    if model == "disk":
+        return torch.log((1 + z.abs()) / (1 - z.abs()))
+    
+    elif model == "plane":
+        raise NotImplementedError()
+
+    else:
+        raise NotImplementedError("The model must be the Poincare Disk ('disk') or Lobachevksy half plane('plane')")
+    
+
+def moebius(z: TensorType[1], p: TensorType[1], model="disk"):
+    """A Möbius transformation that sends p to the center and is distance preserving.
+
+    Args:
+        z (complex tensor): 
+                    Specifies the point that gets transformed.
+        p (complex tensor): 
+                    Specifies the point which is sent to teh center by the transformation.
+    
+    Returns:
+        distance (float): 
+                    The transformed point z.
+    """
+    # ensure that numbers are complex
+    if z.dtype is not torch.complex64 or p.dtype is not torch.complex64:
+        raise TypeError("Both numbers must be complex!")
+    
+    if model == "disk":
+        return (z - p) / (1 - torch.conj(p)*z)
+    
+    elif model == "plane":
+        raise NotImplementedError()
+
+    else:
+        raise NotImplementedError("The model must be the Poincare Disk ('disk') or Lobachevksy half plane('plane')")
