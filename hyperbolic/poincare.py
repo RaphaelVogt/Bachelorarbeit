970db6bca9d02c1aff1b464061edda26a86ebaab
Raphael Vogt <raphael.vogt@gmx.net>
2024-01-30 15:35:57 +0100

    added two functions for the exponential map and used it to create plots

--- hyperbolic/poincare.py
+++ hyperbolic/poincare.py
@@ -3,7 +3,7 @@ from torchtyping import TensorType
 
 
 def distance(z1: TensorType[1], z2: TensorType[1], model="disk"):
-    """Computes the Riemann distance between two points in the Poincare Disk
+    """Computes the Riemann distance between two points in the hyperbolic model.
 
     Args:
         z1 (complex tensor): 
@@ -31,7 +31,7 @@ def distance(z1: TensorType[1], z2: TensorType[1], model="disk"):
 
 
 def geodesic(z1: TensorType[1], z2: TensorType[1], t, model="disk"):
-    """Computes the geodesic, which is the infimum over all curves between the points
+    """Computes the geodesic, which is the infimum over all curves between the points.y
 
     Args:
         z1 (complex tensor): 
@@ -62,7 +62,7 @@ def geodesic(z1: TensorType[1], z2: TensorType[1], t, model="disk"):
 
 
 def distance_center(z: TensorType[1], model="disk"):
-    """Computes the Riemann distance between two points in the Poincare Disk
+    """Computes the hyperbolic Riemannian Distance to the center.
 
     Args:
         z1 (complex tensor): 
@@ -106,6 +106,71 @@ def moebius(z: TensorType[1], p: TensorType[1], model="disk"):
     
     if model == "disk":
         return (z - p) / (1 - torch.conj(p)*z)
+    
+    elif model == "plane":
+        raise NotImplementedError()
+
+    else:
+        raise NotImplementedError("The model must be the Poincare Disk ('disk') or Lobachevksy half plane('plane')")
+    
+    
+def exp_map(z: TensorType[1], v: TensorType[1], model="disk"):
+    """Computes the exponential map on the Poincaré Disk.
+
+    Args:
+        z (complex tensor): 
+                    The starting point on the hyperbolic model.
+        v (complex tensor):
+                    The Tangent vector.
+
+    Returns:
+        z_new (complex tensor):
+                    The pont on the hyperbolic model after the map.
+    """
+    # ensure that numbers are complex
+    if z.dtype is not torch.complex64 or z.dtype is not torch.complex64:
+        raise TypeError("Both numbers must be complex!")
+    
+    # introduce the operations on the poincare disk
+    circ_cross = lambda v, t : torch.tanh(t*torch.atanh(torch.norm(v))) * (v / torch.norm(v))
+
+    if model == "disk":
+        return circ_cross(z, (torch.tanh(torch.norm(v) / (1 - torch.norm(z)**2)) * (v / torch.norm(v))))
+    
+    elif model == "plane":
+        raise NotImplementedError()
+
+    else:
+        raise NotImplementedError("The model must be the Poincare Disk ('disk') or Lobachevksy half plane('plane')")
+    
+
+def exp_map_2(z: TensorType[1], v: TensorType[1], model="disk"):
+    """Computes the exponential map on the Poincaré Disk.
+
+    Args:
+        z (complex tensor): 
+                    The starting point on the hyperbolic model.
+        v (complex tensor):
+                    The Tangent vector.
+
+    Returns:
+        z_new (complex tensor):
+                    The pont on the hyperbolic model after the map.
+    """
+    # ensure that numbers are complex
+    if z.dtype is not torch.complex64 or z.dtype is not torch.complex64:
+        raise TypeError("Both numbers must be complex!")
+    
+    if model == "disk":
+        theta = torch.angle(v)
+        s = 2*torch.abs(v) / (1 - torch.abs(z)**2)
+
+        exp_i_theta = torch.exp(1j * theta)
+        exp_minus_s = torch.exp(-s)
+
+        num = z + exp_i_theta + (z - exp_i_theta) * exp_minus_s
+        den = 1 + torch.conj(z) * exp_i_theta + (1 - torch.conj(z) * exp_i_theta) * exp_minus_s
+        return num / den
     
     elif model == "plane":
         raise NotImplementedError()
