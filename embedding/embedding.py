e194cef429728f7a48b2878e4a858ee6810f8cac
Raphael Vogt <raphael.vogt@gmx.net>
2024-03-05 13:54:58 +0100

    adjusted rsgd for normalized and average distortion

--- embedding/embedding.py
+++ embedding/embedding.py
@@ -3,8 +3,10 @@ import math
 
 from .graph import Graph
 from .poincare import PoincareDisk
+from .image_creation_funcs import geodesic
 from torchtyping import TensorType
 from IPython.display import clear_output
+from matplotlib import pyplot as plt
 
 class Embedding:
     def __init__(self, graph: Graph, disk: PoincareDisk):
@@ -14,6 +16,39 @@ class Embedding:
         self._graph = graph
         self._disk = disk
         self._num_points = graph._num_nodes
+
+
+    def visualize(self, size_point: float, size_fig: float, show_circle: bool, show_numbers: bool, show_geodesics: bool):
+        """ Visualizes the Poincare disk with the geodesics between the points.
+
+        Args:
+            size_point (float):
+                            The size of the scattered data points.
+            size_fig (float):
+                            The size of the pyplot figure.
+            show_circle (bool):
+                            Specifies of partialD should be also plotted.
+            show_numbers (bool):
+                            Specifies of the data points are numbered.
+            show_geodesics (bool):
+                            Specifies if the data points are connected with geodesics.  
+        """
+        fig, ax = plt.subplots(figsize=(size_fig, size_fig))
+        if show_circle:
+            x_sphere = torch.cos(torch.linspace(0, 2 * math.pi, 1000))
+            y_sphere = torch.sin(torch.linspace(0, 2 * math.pi, 1000))
+            ax.plot(x_sphere, y_sphere)
+        if show_geodesics:
+            ts = torch.linspace(0, 1, 100)
+            for u, v in self._graph._graph.iterEdges():
+                geodesic_points = geodesic(self._disk.data_points[u].unsqueeze(0), self._disk._data_points[v].unsqueeze(0), ts)
+                ax.plot(geodesic_points.real, geodesic_points.imag)
+        ax.scatter(self._disk._data_points.real, self._disk._data_points.imag, color="black", s=size_point)
+        if show_numbers:
+            for i in range(self._num_points):
+                ax.text(self._disk._data_points[i].real, self._disk._data_points[i].imag, i)
+        plt.axis("off")
+        plt.show()
 
 
     def pairwise_distortion(self, index_0: int, index_1: int):
@@ -55,6 +90,10 @@ class Embedding:
     def normal_avg_distortion_vec(self, data_points: TensorType["Number of elements"]):
         """ Computes the normalized average distortion vectorized.
 
+        Args:
+            data_points (complex tensor):
+                        Torch.tensor containing the data points.
+
         Returns:
             avg_distortion (float):
                         Normalized average distortion.
@@ -77,13 +116,24 @@ class Embedding:
     
 
     def autograd_normal_avg_distortion_vec(self, data_points: TensorType["Number of elements"]):
+        """ Computes the gradient of the normalized average distortions using autograd.
+        
+        Args:
+            data_points (complex tensor):
+                        Torch.tensor containing the data points.
+
+        Returns:    
+            grads (complex tensor):
+                        The computed gradients.
+        """
+        data_points = data_points.detach().clone().requires_grad_(True)
         distortion = self.normal_avg_distortion_vec(data_points)
         distortion.backward()
         return data_points.grad
     
 
     def compute_alpha(self):
-        """ Computes the normalized average distortion vectorized.
+        """ Computes alpha for the gradient of normalized average distortion.
 
         Returns:
             avg_distortion (float):
@@ -106,14 +156,14 @@ class Embedding:
     
 
     def grad_normal_avg_distortion_vec(self, index: int):
-        """ Computes the gradient of the distortion of one point.
+        """ Computes the gradient of the normalized distortion of one point.
         
         Args:
             index (int):
                         The index of the point.
 
         Returns:    
-            grad (float):
+            grad (complex):
                         The computed gradient.
         """
         n = self._num_points
@@ -129,30 +179,98 @@ class Embedding:
         distances_graph = torch.cat((distances_graph[index, 0:index], distances_graph[index, index+1:]))
 
         return (2 / (n*(n-1))) * torch.sum(grad_distances_disk / (distances_graph * alpha))
+    
 
+    def avg_distortion_vec(self, data_points: TensorType["Number of elements"]):
+        """ Computes the average distortion vectorized.
 
-    def line_search(self, riemannian_grad: complex, index: int):
+        Args:
+            data_points (complex tensor):
+                        Torch.tensor containing the data points.
+
+        Returns:
+            avg_distortion (float):
+                        Normalized average distortion.
+        """
+        n = self._num_points
+
+        # compute distances in the disk for all possible combinations
+        combs = torch.combinations(torch.arange(n))
+        distances_disk = self._disk.riemann_distance_vec(data_points[combs[:, 0]], data_points[combs[:, 1]])
+
+        # filter the distances in the graph s.t. no duplicates are inside
+        distances_graph = self._graph.distance_all()
+        distances_graph = torch.flatten(torch.triu(distances_graph))
+        distances_graph = distances_graph[distances_graph.nonzero()].reshape(combs.shape[0])
+
+        return (2 / (n * (n-1))) * torch.sum(torch.abs(distances_disk - distances_graph) / distances_graph)
+    
+
+    def autograd_avg_distortion_vec(self, data_points: TensorType["Number of elements"]):
+        """ Computes the gradient of the average distortions using autograd.
+        
+        Args:
+            data_points (complex tensor):
+                        Torch.tensor containing the data points.
+
+        Returns:    
+            grads (complex tensor):
+                        The computed gradients.
+        """
+        data_points = data_points.detach().clone().requires_grad_(True)
+        distortion = self.avg_distortion_vec(data_points)
+        distortion.backward()
+        return data_points.grad
+    
+    
+    def grad_avg_distortion_vec(self, index: int):
+        """ Computes the gradient of the average distortion of one point.
+        
+        Args:
+            index (int):
+                        The index of the point.
+
+        Returns:    
+            grad (float):
+                        The computed gradient.
+        """
+        n = self._num_points
+
+        theta = self._disk._data_points[index].detach().clone()
+        thetas = torch.ones(n-1, dtype=torch.complex128) * theta
+        combs = torch.cat((torch.arange(index), torch.arange(index+1, n)))
+
+        grad_distances_disk = self._disk.grad_riemann_distance_vec(thetas, self._disk._data_points[combs])
+        distances_disk = self._disk.riemann_distance_vec(thetas, self._disk._data_points[combs])
+
+        distances_graph = self._graph.distance_all()
+        distances_graph = torch.cat((distances_graph[index, 0:index], distances_graph[index, index+1:]))
+
+        return (2 / (n*(n-1))) * torch.sum(((distances_disk - distances_graph) * grad_distances_disk) / (distances_graph * torch.abs(distances_disk - distances_graph)))
+    
+
+    def line_search(self, riemannian_grad: complex, index: int, distortion_func):
         """ Computes a suitable learning rate using the Armijo condition.
 
         Args:
-            riemannian_grad_batch (complex):
-                        Riemannian gradient of loss over batch.
-            index_start (int):
-                        Start index of data batch.
-            index_end (ind):
-                        End index of data batch.
+            riemannian_grad (complex):
+                        Riemannian gradient of loss.
+            index (int):
+                        Index of element which gets mapped.
+            distortion_func (function):
+                        The function used to compute the distortion.
         
         Returns:
-            eta (float):
+            learning_rate (float):
                         A suitable learning rate which gurantees a sufficient improvement.
         """
         learning_rate = 2
-        tau = 0.5
-        r = 1e-5
+        tau = .5
+        r = 1e-4
 
         original_thetas = self._disk._data_points.detach().clone()
         original_theta = self._disk._data_points[index].detach().clone().unsqueeze(0)
-        old_distortion = self.normal_avg_distortion_vec(original_thetas)
+        old_distortion = distortion_func(original_thetas)
 
         # initial value to force while loop
         new_distortion = old_distortion.detach().clone()
@@ -161,29 +279,60 @@ class Embedding:
             learning_rate *= tau
             mapped_theta = self._disk.exp_map(original_theta, -learning_rate*riemannian_grad)
             original_thetas[index] = mapped_theta
-            new_distortion = self.normal_avg_distortion_vec(original_thetas)
-            if math.isnan(new_distortion):
+            new_distortion = distortion_func(original_thetas)
+            if math.isnan(new_distortion) or math.isinf(new_distortion):
                 learning_rate *= tau
                 new_distortion = old_distortion.detach().clone()
                 continue
 
+            if learning_rate < 1e-300:
+                return 0
+ 
         return learning_rate
+    
 
-
-    def RSGD(self, num_epochs: int):
+    def RSGD(self, num_epochs: int, autograd: bool, distortion: str):
+        # save distortions during optimization
         distortion_per_epoch = torch.zeros(num_epochs+1)
-        distortion_per_epoch[0] = self.normal_avg_distortion_vec(self._disk._data_points)
+        if distortion == "avg":
+            distortion_per_epoch[0] = self.avg_distortion_vec(self._disk._data_points)
+        elif distortion == "normal_avg":
+            distortion_per_epoch[0] = self.normal_avg_distortion_vec(self._disk._data_points)
+        else:
+            raise RuntimeError("Invalid distortion selected!")
+        
         for epoch in range(num_epochs):
             for i in torch.randperm(self._num_points).tolist():
-                #euclid_grads = self.autograd_normal_avg_distortion_vec(thetas)
-                euclid_grad = self.grad_normal_avg_distortion_vec(i)
+                # compute euclidean gradient depending on mode
+                if autograd and distortion == "avg":
+                    euclid_grad = self.autograd_avg_distortion_vec(self._disk._data_points)[i].unsqueeze(0)
+                elif not autograd and distortion == "avg":
+                    euclid_grad = self.grad_avg_distortion_vec(i).unsqueeze(0)
+                elif autograd and distortion == "normal_avg":
+                    euclid_grad = self.autograd_normal_avg_distortion_vec(self._disk._data_points)[i].unsqueeze(0)
+                elif not autograd and distortion == "normal_avg":
+                    euclid_grad = self.grad_normal_avg_distortion_vec(i).unsqueeze(0)
+
+                # resclae it in length to riemannian gradient
                 theta = self._disk._data_points[i].detach().clone().unsqueeze(0)
-                riemannian_grad_theta = ((1 - torch.norm(theta)**2) / 2) * euclid_grad.unsqueeze(0)
-                learning_rate = self.line_search(riemannian_grad_theta, i)
+                riemannian_grad_theta = ((1 - torch.norm(theta)**2) / 2) * euclid_grad
+
+                # choose a suitable learning rate using line search
+                if distortion == "avg":
+                    learning_rate = self.line_search(riemannian_grad_theta, i, self.avg_distortion_vec)
+                elif distortion == "normal_avg":
+                    learning_rate = self.line_search(riemannian_grad_theta, i, self.normal_avg_distortion_vec)
+
+                # update formula
                 self._disk._data_points[i] = self._disk.exp_map(theta, -learning_rate*riemannian_grad_theta)
+            
+            if distortion == "avg":
+                distortion_per_epoch[epoch+1] = self.avg_distortion_vec(self._disk._data_points)
+                clear_output(wait=False)
+                print(f"Average distortion after {epoch+1}. epoch: {self.avg_distortion_vec(self._disk._data_points)}.")
+            elif distortion == "normal_avg":
+                distortion_per_epoch[epoch+1] = self.normal_avg_distortion_vec(self._disk._data_points)
+                clear_output(wait=False)
+                print(f"Normalized average distortion after {epoch+1}. epoch: {self.normal_avg_distortion_vec(self._disk._data_points)}.")
 
-            clear_output(wait=False)
-            print(f"Distortion after {epoch+1}. epoch: {self.normal_avg_distortion_vec(self._disk._data_points)}.")
-
-            distortion_per_epoch[epoch+1] = self.normal_avg_distortion_vec(self._disk._data_points)
         return distortion_per_epoch
